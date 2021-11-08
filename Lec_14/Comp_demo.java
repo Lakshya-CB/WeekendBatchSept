aed4097fbed980c8c801295e42151d2069d557f4
Lakshya-CB <78645656+Lakshya-CB@users.noreply.github.com>
2021-11-08 15:18:51 +0530

    Add files via upload

---
+++ Lec_14/Comp_demo.java
@@ -0,0 +1,12 @@
+package Lec_14;
+
+public class Comp_demo {
+	public static void main(String[] args) {
+
+		int n = 10;
+		for (int i = 0; i < n; i++) {
+			System.out.println("lolW");
+			System.out.println("lolW");
+		}
+	}
+}
